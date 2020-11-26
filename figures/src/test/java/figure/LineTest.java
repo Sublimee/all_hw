84c8a4398922b7b0301396d6fa21c7c56efe903f
Maksim Gusev <aquariusborelias@gmail.com>
2020-11-26 19:43:39 +0300

    line test added

--- figures/src/test/java/figure/LineTest.java
+++ figures/src/test/java/figure/LineTest.java
@@ -6,10 +6,22 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 public class LineTest {
 
     @Test
     public void lineConstructorTest() {
+        Point point = new Point(0, 0);
+        Point anotherPoint = new Point(0, 1);
+        List<Point> points = Arrays.asList(point, anotherPoint);
+        Line line = new Line(points);
+        assertEquals(line.getPoints().get(0), point);
+        assertEquals(line.getPoints().get(1), anotherPoint);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void lineConstructorDiagonalTest() {
         List<Point> points = Arrays.asList(new Point(0, 0), new Point(1, 1));
         new Line(points);
     }
